d1a5f054b188686c78c104d27d19f6244e0eb54a
DanielleWhanau <danielle.whanau@harcourts.net>
2019-10-14 16:15:36 +1300

    added comments to code + added rental listing codes

--- cypress/support/commands/Listing.js
+++ cypress/support/commands/Listing.js
@@ -13,6 +13,7 @@ Cypress.Commands.add('addListing', (type) => {
         var sellerModal = '#divContactNewQuick > .modalBody'
         var selectTab = '.ajax__tab_header span.ajax__tab_tab'
         var command = cy
+            //Selects Listing Consultant
             .get('[id$=ajaxSearchStaff_hidSelectedID] > .select2-choice > .select2-arrow')
             .click()
             .get('[id$=select2-drop] > .select2-search > input')
@@ -21,9 +22,11 @@ Cypress.Commands.add('addListing', (type) => {
             .get(selectFromList)
             .contains(agentName)
             .click()
+            //Selects Address sub tab
             .get(selectTab)
             .contains('Address')
             .click()
+            //Enters Address details
             .get('[id$=txtStreetNumber]')
             .click()
             .type(streetNumber)
@@ -39,23 +42,28 @@ Cypress.Commands.add('addListing', (type) => {
             .get(selectFromList, { timeout: 2000 })
             .contains(suburb, { timeout: 2000 })
             .click()
+            //Selects Map sub tab
             .get(selectTab)
             .contains('Map')
             .click()
+            //Selects the possible map location
             .get(".listing-edit-map", { timeout: 30000 })
             .should("be.visible")
             .get('#geocodeResults > a')
             .contains("New Zealand")
             .click()
+            //Selects the Seller sub tab
             .get(selectTab)
             .contains('Sellers')
             .click()
+            //Adds a Seller/Owner via the Add new button
             .get('[id$=btnAddContact]', { timeout: 2000 })
             .click()
             .get(sellerModal)
             .should('be.visible')
             .get('[id$=txtFirstName]', { timeout: 2000 })
             .click()
+            //Enters seller/owner details
             .type(firstName)
             .get('[id$=txtLastName]')
             .click()
@@ -70,9 +78,13 @@ Cypress.Commands.add('addListing', (type) => {
             .click()
             .get(sellerModal)
             .should('not.be.visible')
+            //Selects the Listing sub tab
             .get('[id$=tab4_tab]')
             .click()
-            .get('[id$=radStatusAvailable]')
+            .get('.spinner')
+            .should('not.be.visible')
+            //Sets 'Available' status
+            .get('[id$=radStatusAvailable]', { timeout: 2000 })
             .click()
 
 
@@ -82,17 +94,21 @@ Cypress.Commands.add('addListing', (type) => {
             command.get('[id$=advMinimumPrice_0]')
                 .click()
                 .type('1500000')
+                //Selects Property Type sub tab
                 .get(selectTab)
                 .contains('Property Type')
                 .click()
                 .get('[id$=advPropertyType_7_0]')
                 .click()
+                //Sets Bedrooms
                 .get('[id$=advBedrooms_0]')
                 .click()
                 .type('4')
+                //Sets Bathrooms
                 .get('[id$=advBathrooms_0]')
                 .click()
                 .type('2')
+                //Sets Lounge Rooms
                 .get('[id$=advLoungeRooms_0]')
                 .click()
                 .type('2')
@@ -100,12 +116,28 @@ Cypress.Commands.add('addListing', (type) => {
         }
 
         if (type === 'residential-rental') {
-            //Selects Full edit listing permissions
-            command.get('span[securityroleid=18] > input')
+            //Selects Rent Rate Field
+            command.get('[id$=txt_advRent_0]')
                 .click()
-                //Selects Selling Consultant permissions
-                .get('span[securityroleid=29] > input')
+                .type('500')
+                //Selects Property Type sub tab
+                .get(selectTab)
+                .contains('Property Type')
                 .click()
+                .get('[id$=advPropertyType_7_0]')
+                .click()
+                //Sets Bedrooms
+                .get('[id$=advBedrooms_0]')
+                .click()
+                .type('4')
+                //Sets Bathrooms
+                .get('[id$=advBathrooms_0]')
+                .click()
+                .type('2')
+                //Sets Lounge Rooms
+                .get('[id$=advLoungeRooms_0]')
+                .click()
+                .type('2')
         }
 
         command
@@ -117,34 +149,35 @@ Cypress.Commands.add('addListing', (type) => {
 
 Cypress.Commands.add('Publishing', () => {
     var randomText = 'Check out this super cool property weeow!'
+    //Selects Publishing tab
     cy.get('[id$=tc_tab04]')
         .contains('Publishing')
         .click()
         .get('.spinner')
         .should('not.be.visible')
-        .get('[id$=advInternetHeading_0]')
+        //Selects and enters listing header
+        .get('[id$=advInternetHeading_0]', { timeout: 20000 })
+        .should('be.visible')
         .click()
         .type('This home is GUCCI!')
+        //Selects and enters listing body
         .get('[id$=dvInternetBody_0]')
         .click()
         .type(randomText)
-        .get('[id$=ctl02_rbUseExport]')
-        .click()
-        .get('[id$=ctl03_rbUseExport]')
-        .click()
-        .get('[id$=pnlExportToTradeMeHeader]')
-        .should('be.visible')
-        .get('[id$=btnYesExportToTradeMe]')
-        .click()
+
 });
 
 Cypress.Commands.add('SaveListing', () => {
+    //Selects save button
     cy.get('[id$=uclEditSave_btnSave]')
         .contains('Save Now')
         .click()
+        .get('.spinner')
+        .should('not.be.visible')
 });
 
 Cypress.Commands.add('ViewListing', () => {
+    //Selects View listing button
     cy.get('[id$=hypViewListingPageControl]')
         .contains('View listing')
         .click()
