79fd0858881f7e6c16f30fb2f086188ac5933e57
DanielleWhanau <danielle.whanau@harcourts.net>
2019-10-14 16:24:11 +1300

    incorrect name

--- cypress/integration/createListing/residentialRental.spec.js
+++ cypress/integration/createListing/residentialRental.spec.js
@@ -1,16 +1,16 @@
 import Environment from "../../support/Environment";
 
-describe('Create a Residential Sales Listing', function () {
+describe('Create a Residential Rental Listing', function () {
 
     beforeEach(() => {
         cy.visit(Environment.pages.dashboard);
         cy.login();
-        cy.visit(Environment.pages.residentialSalesListing);
-        cy.addListing('residential-sales');
+        cy.visit(Environment.pages.residentialRentalListing);
+        cy.addListing('residential-rental');
         
     });
 
-    it('can create minimal residential sales listing', () => {
+    it('can create minimal residential rental listing', () => {
         cy.get('@users').then(({ firstName, lastName }) => {
         })
     })
