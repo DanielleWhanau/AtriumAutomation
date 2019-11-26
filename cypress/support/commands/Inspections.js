72fe1b05111d4417098a7016b9ae3daa6919b499
DanielleWhanau <danielle.whanau@harcourts.net>
2019-11-27 10:44:48 +1300

    Tweaked inspections file

--- cypress/support/commands/Inspections.js
+++ cypress/support/commands/Inspections.js
@@ -10,38 +10,38 @@ Cypress.Commands.add('addInspections', (type) => {
         cy.get('[id$=tabInspection_tab]')
             .click( { force: true} )
             //Select Add New button on page
-            .get('[id$=uclListingInspectionListEdit_hypAddNewInspection]')
-            .click( { force: true} )
+            .get('[id$=uclListingInspectionListEdit_hypAddNewInspection]', { timeout: 5000 })
+            .click()
             //Enter Inspection date
-            .get('[id$=uclNewListingInspectionEdit_dtDateInspectedDateTime_txtDate]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_dtDateInspectedDateTime_txtDate]', { timeout: 5000 })
+            .click()
             .type(futureDate)
             //Add Contact - via Add New Contact option
-            .get('[id$=uclNewListingInspectionEdit_btnAddContact]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_btnAddContact]', { timeout: 5000 })
+            .click()
             .get('.spinner')
             .should('not.be.visible')
             //Enter contact first name
-            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtFirstName]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtFirstName]', { timeout: 5000 })
+            .click()
             .type(firstName)
             //Enter contact last name
-            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtLastName]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtLastName]', { timeout: 5000 })
+            .click()
             .type(lastName)
             //Enter contact number
-            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtHomeNumber]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_uclContactNewQuick_txtHomeNumber]', { timeout: 5000 })
+            .click()
             .type('022 2222222')
             //Save contact
-            .get('[id$=tabInspection_uclListingInspectionListEdit_uclNewListingInspectionEdit_uclContactNewQuick_btnAdd]')
-            .click( { force: true} )
+            .get('[id$=tabInspection_uclListingInspectionListEdit_uclNewListingInspectionEdit_uclContactNewQuick_btnAdd]', { timeout: 5000 })
+            .click()
             //Add Inspection comment
-            .get('[id$=uclNewListingInspectionEdit_txtComment]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_txtComment]', { timeout: 5000 })
+            .click()
             .type(text)
             //Save Inspection
-            .get('[id$=uclNewListingInspectionEdit_btnAdd]')
-            .click( { force: true} )
+            .get('[id$=uclNewListingInspectionEdit_btnAdd]', { timeout: 5000 })
+            .click()
     })
 });
