f9ea5b34ab3de5483529ef38ed8ea6020084773e
DanielleWhanau <danielle.whanau@harcourts.net>
2020-04-08 15:22:09 +1200

    minor changes

--- cypress/support/commands/DeleteEnquiries.js
+++ cypress/support/commands/DeleteEnquiries.js
@@ -1,10 +1,17 @@
+import Environment from "../../support/Environment";
+
 Cypress.Commands.add('deleteEnquiries', (type) => {
     return cy.then(() => {
         //Adds Enquiries to my listing
         cy.get('[id$=tabEnquiries_tab]')
             .click()
             //Select Delete (on random enquiry)
-            .get('[id$=uclMatchedListingEnquiries_rptEnquiries_ctl02_lnkDelete]')
+            .get('.enquiries-column .icon-delete')
+            .last()
+            .click({ force: true })
+            .wait(5000)
+            .visit(Environment.pages.residentialSalesListing)
+            .get('[id$=tabEnquiries_tab]')
             .click()
     })
 });
